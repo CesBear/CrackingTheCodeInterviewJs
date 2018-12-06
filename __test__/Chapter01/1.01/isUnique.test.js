7b03a710d70b2a8547da40004696eade1f8515ce
Cesar Ramirez <carr.gdl1@gmail.com>
2018-12-05 20:49:33 -0600

    adding jest conf and first set of tests

---
+++ __test__/Chapter01/1.01/isUnique.test.js
@@ -0,0 +1,17 @@
+const uniqueChars = require('../../../exercises/Chapter01/1.01/isUnique');
+
+test('Validate that chars are not unique result is false', () => {
+    expect(uniqueChars("ana")).toBe(false);
+});
+
+test('Validate unique chars ', () => {
+    expect(uniqueChars("ano")).toBe(true);
+});
+
+test('Validate numbers', () => {
+    expect(uniqueChars("unos")).toBe(true);
+});
+
+test('Validate white spaces not allowed or validated', () => {
+    expect(uniqueChars("unos lo")).toBe(false);
+});
