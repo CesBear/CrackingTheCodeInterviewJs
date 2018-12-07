ab890a3e2f33057f3e3a34c3cac7e42450792219
Cesar Ramirez <carr.gdl1@gmail.com>
2018-12-06 21:52:48 -0600

    updating ex 3

--- __test__/Chapter01/1.03/URLify.test.js
+++ __test__/Chapter01/1.03/URLify.test.js
@@ -11,15 +11,19 @@ test('3 spaces and 14 as a number', () => {
 test('Two char word and 7 as a number', () => {
     expect(urlifyChecker('   hi', 7)).toEqual('%20%20%20hi%20%20');
 });
+
 test('Two char word and 3 as a number', () => {
     expect(urlifyChecker('   hi ', 3)).toEqual('%20hi');
 });
+
 test('No spaces', () => {
     expect(urlifyChecker('', 0)).toEqual('');
 });
+
 test('NO spaces and 2 as a number', () => {
     expect(urlifyChecker('', 2)).toEqual('%20%20');
 });
+
 test('1 space and 2 words', () => {
     expect(urlifyChecker('hel lo', 5)).toEqual('hello');
 });
--- exercises/Chapter01/1.03/URLify.js
+++ exercises/Chapter01/1.03/URLify.js
@@ -1,10 +1,23 @@
 /*
 Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the
 end to hold the additional characters, and that you are given the "true" length of the string.
+
+I (input): Sting, number
+O (output): String
+C (constrains): optimize
+E (Edge cases): empty string, spaces in front, middle and end
+
  */
+
 
 const urlifyChecker = (str, n = str.length) => {
 
+//first pass: count the number of non space chars in the string
+//subtract chars from true length n to se how many spaces we are allow to replace with  %20
+
+//second pass: if we see a space and there are still spaces left, append %20 to output string
+//otherwise copy current character
+//when run out of spaces, append the empty string
     let out = '';
     let chars = 0;
 
